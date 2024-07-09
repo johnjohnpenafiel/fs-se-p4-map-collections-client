e7a019a15a7ccd139f30e97c85bbb378e6567a80
Guang <jouyang19@proton.me>
2024-07-09 17:25:56 -0400

    create login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ function Login({ setLoggedIn, loggedIn, user, setUser }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
+  const [signUp, setSignUp] = useState(false);
 
   function validateForm(data) {
     const errors = { name: "", pass: "" };
@@ -34,7 +35,7 @@ function Login({ setLoggedIn, loggedIn, user, setUser }) {
     });
 
     if (validationErrors.name === "" && validationErrors.pass === "") {
-      fetch("/login", {
+      fetch("http://localhost:4000/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -45,60 +46,135 @@ function Login({ setLoggedIn, loggedIn, user, setUser }) {
           password: password,
         }),
       });
+      console.log("Sign-in Successful");
     }
   }
 
+  function handleSignUpButton(event) {
+    event.preventDefault();
+    setSignUp(!signUp);
+  }
+
+  if (signUp === true) {
+    return (
+      <>
+        <form className="mt-8 mx-8 space-y-6" onSubmit={handleSubmit}>
+          <input defaultValue="true" name="remember" type="hidden" />
+          <div className="-space-y-px rounded-md shadow-sm">
+            <div>
+              <label className="sr-only" htmlFor="username">
+                Username
+              </label>
+              <input
+                autoComplete="username"
+                className="relative block w-full appearance-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                id="username"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+                required
+                type="text"
+              />
+            </div>
+            <div>
+              <label className="sr-only" htmlFor="password">
+                Password
+              </label>
+              <input
+                autoComplete="current-password"
+                className="relative block w-full appearance-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+                type="password"
+              />
+            </div>
+          </div>
+          <div className="text-red-600">
+            <p>{errors.name && <p>{errors.name}</p>}</p>
+            <p>{errors.pass && <p>{errors.pass}</p>}</p>
+          </div>
+          <div className="pt-4">
+            <button
+              className="group relative flex w-full justify-center rounded-md border border-white bg-black py-2 px-4 text-sm font-medium text-white hover:bg-gray focus:outline-none focus:ring-2 focus:ring-3 focus:ring-offset-2"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+        <div className="mx-8">
+          <p>Already have an account?</p>
+          <button type="submit" onClick={handleSignUpButton}>
+            Log In
+          </button>
+        </div>
+      </>
+    );
+  }
+
   return (
-    <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-      <input defaultValue="true" name="remember" type="hidden" />
-      <div className="-space-y-px rounded-md shadow-sm">
-        <div>
-          <label className="sr-only" htmlFor="username">
-            Username
-          </label>
-          <input
-            autoComplete="username"
-            className="relative block w-full appearance-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-            id="username"
-            name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-            required
-            type="text"
-          />
+    <>
+      <form className="mt-8 mx-8 space-y-6" onSubmit={handleSubmit}>
+        <input defaultValue="true" name="remember" type="hidden" />
+        <div className="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label className="sr-only" htmlFor="username">
+              Username
+            </label>
+            <input
+              autoComplete="username"
+              className="relative block w-full appearance-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+              required
+              type="text"
+            />
+          </div>
+          <div>
+            <label className="sr-only" htmlFor="password">
+              Password
+            </label>
+            <input
+              autoComplete="current-password"
+              className="relative block w-full appearance-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              required
+              type="password"
+            />
+          </div>
         </div>
-        <div>
-          <label className="sr-only" htmlFor="password">
-            Password
-          </label>
-          <input
-            autoComplete="current-password"
-            className="relative block w-full appearance-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            required
-            type="password"
-          />
+        <div className="text-red-600">
+          <p>{errors.name && <p>{errors.name}</p>}</p>
+          <p>{errors.pass && <p>{errors.pass}</p>}</p>
         </div>
-      </div>
-      <div className="text-red-600">
-        <p>{errors.name && <p>{errors.name}</p>}</p>
-        <p>{errors.pass && <p>{errors.pass}</p>}</p>
-      </div>
-      <div className="pt-4">
-        <button
-          className="group relative flex w-full justify-center rounded-md border border-white bg-black py-2 px-4 text-sm font-medium text-white hover:bg-gray focus:outline-none focus:ring-2 focus:ring-3 focus:ring-offset-2"
-          type="submit"
-        >
-          <span className="absolute inset-y-0 left-0 flex items-center pl-3"></span>
-          Log in
+        <div className="pt-4">
+          <button
+            className="group relative flex w-full justify-center rounded-md border border-white bg-black py-2 px-4 text-sm font-medium text-white hover:bg-gray focus:outline-none focus:ring-2 focus:ring-3 focus:ring-offset-2"
+            type="submit"
+          >
+            Log in
+          </button>
+        </div>
+      </form>
+      <div className="mx-8">
+        <p>Don't have an account?</p>
+        <button type="submit" onClick={handleSignUpButton}>
+          Sign Up
         </button>
       </div>
-    </form>
+    </>
   );
 }
 
